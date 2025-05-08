4067795191209edc6c269439cbc05cb56e1f7d73
Md.Mainul Hasan <mdevhasan@gmail.com>
2025-05-08 17:18:04 +0600

    Add catchAsyn is all controller

--- user-service/src/controllers/user.controller.js
+++ user-service/src/controllers/user.controller.js
@@ -1,41 +1,30 @@
+// controllers/user.controller.js
+import { catchAsync } from '../utils/catchAsync.js';
 import {
   findUserByIdUserService,
   findUserService,
-  registerUser
+  registerUser,
 } from '../services/user.service.js';
 import { UserDTO } from '../dto/user.dto.js';
 import { logger } from '../utils/logger.js';
 
-export const registerController = async (req, res, next) => {
-  try {
-    logger.info('Register Controller Called...');
-    const { name, email, password } = req.body;
-    const userDTO = new UserDTO(name, email, password);
-    const newUser = await registerUser(userDTO);
-    res.status(201).json({ message: 'User created successfully', user: newUser });
-  } catch (error) {
-    next(error);
-  }
-};
+export const registerController = catchAsync(async (req, res) => {
+  logger.info('Register Controller Called...');
+  const { name, email, password } = req.body;
+  const userDTO = new UserDTO(name, email, password);
+  const newUser = await registerUser(userDTO);
+  res.status(201).json({ message: 'User created successfully', user: newUser });
+});
 
-export const findUsersController = async (req, res, next) => {
-  try {
-    logger.info('findUsers Controller Called...');
-    const users = await findUserService();
-    res.status(200).json({ message: 'Users fetched successfully', user: users });
-  } catch (error) {
-    next(error);
-  }
-};
+export const findUsersController = catchAsync(async (req, res) => {
+  logger.info('findUsers Controller Called...');
+  const users = await findUserService();
+  res.status(200).json({ message: 'Users fetched successfully', user: users });
+});
 
-export const findUserByIdController = async (req, res, next) => {
+export const findUserByIdController = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    logger.info(`Finding user with ID: ${id}`);
-    const user = await findUserByIdUserService(id);
-    res.status(200).json({ message: 'User found successfully', user });
-  } catch (error) {
-    logger.error(`Error while fetching user with ID: ${id} - ${error.message}`);
-    next(error);
-  }
-};
+  logger.info(`Finding user with ID: ${id}`);
+  const user = await findUserByIdUserService(id);
+  res.status(200).json({ message: 'User found successfully', user });
+});
